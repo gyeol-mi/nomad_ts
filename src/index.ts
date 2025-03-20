a624b1a1fb41eea07b429c42d710ffb90e83286c
emily kang [강경임] <strongwomankky@gmail.com>
2025-03-21 01:54:44 +0900

    7. chain

--- src/index.ts
+++ src/index.ts
@@ -10,9 +10,9 @@ interface BlockShape {
 class Block implements BlockShape {
   public hash: string;
   constructor(
-    public prevHash: string,
-    public height: number,
-    public data: string
+    public readonly prevHash: string,
+    public readonly height: number,
+    public readonly data: string
   ) {
     this.hash = Block.calculateHash(prevHash, height, data);
   }
@@ -22,5 +22,36 @@ class Block implements BlockShape {
   }
 }
 
-const BlockIn = new Block("2", 3, "dsgdsdg");
-console.dir(BlockIn);
+class BlockChain {
+  private blocks: Block[];
+  constructor() {
+    this.blocks = [];
+  }
+  private getPrevHash(): string {
+    return this.blocks.length === 0
+      ? ""
+      : this.blocks[this.blocks.length - 1].hash;
+  }
+  public addBlock(data: string) {
+    const newBlock = new Block(
+      this.getPrevHash(),
+      this.blocks.length + 1,
+      data
+    );
+    this.blocks.push(newBlock);
+  }
+  public getBlocks() {
+    return [...this.blocks];
+  }
+}
+
+const blockchain = new BlockChain();
+
+blockchain.addBlock("First one");
+blockchain.addBlock("Second one");
+blockchain.addBlock("Third one");
+
+const newBlock = blockchain.getBlocks();
+newBlock.push(new Block("xxxx", 1111, "hacked"));
+// console.log(blockchain.getBlocks());
+console.log(newBlock);
